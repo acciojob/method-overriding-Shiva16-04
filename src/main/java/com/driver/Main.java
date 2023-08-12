9f7e70c82cbbaf3f79ad3bd35034232cbcce4ba6
Shiva16-04 <shivakumaryedla1999@gmail.com>
2023-08-12 13:22:18 +0530

    second update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,6 @@ package com.driver;
 public class Main {
     public static void main(String args[]){
         B obj=new B();
-        String s=obj.meth();
-        System.out.println(s);
+        obj.meth();
     }
 }
