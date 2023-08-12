d876269250e424ee82c43761e4a4a1e3dd094f5b
Shiva16-04 <shivakumaryedla1999@gmail.com>
2023-08-12 13:50:43 +0530

    final commit

--- src/main/java/com/driver/B.java
+++
@@ -1,8 +0,0 @@
-package com.driver;
-
-public class B extends A{
-    public String meth(){
-        System.out.println(super.meth());
-        return "Method is overridden in Extendend class B";
-    }
-}
